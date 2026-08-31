48581493b3ead53db8641d79dbce907c8e202233
Your Name <you@example.com>
2026-08-31 08:55:17 -0300

    PokemonAPI com Flutter

---
+++ pokemonapp/lib/providers/pokemon_provider.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../services/pokemon_service.dart';
+
+class PokemonProvider extends ChangeNotifier {
+  List<dynamic> pokemons = [];
+
+  Map<String, dynamic>? pokemonSelecionado;
+
+  bool carregando = false;
+
+  Future<void> carregarPokemons() async {
+    carregando = true;
+
+    notifyListeners();
+
+    pokemons = await PokemonService.buscarPokemons();
+
+    carregando = false;
+
+    notifyListeners();
+  }
+
+  Future<void> carregarDetalhes(
+    int id,
+  ) async {
+    carregando = true;
+
+    pokemonSelecionado = null;
+
+    notifyListeners();
+
+    pokemonSelecionado = await PokemonService.buscarDetalhes(
+      id,
+    );
+
+    carregando = false;
+
+    notifyListeners();
+  }
+
+  Future<void> carregarPesquisa(String pesquisa) async {
+    carregando = true;
+
+    notifyListeners();
+
+    pokemons = await PokemonService.pesquisarPkmn(pesquisa);
+
+    carregando = false;
+
+    notifyListeners();
+  }
+}
---
+++ pokemonapp/lib/services/pokemon_service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class PokemonService {
+  static const String url = 'http://127.0.0.1:5001/pokemons';
+
+  // BUSCAR TODOS
+  static Future<List<dynamic>> buscarPokemons() async {
+    final resposta = await http.get(
+      Uri.parse(url),
+    );
+
+    if (resposta.statusCode == 200) {
+      return jsonDecode(
+        resposta.body,
+      );
+    }
+
+    throw Exception(
+      'Erro ao buscar Pokémon',
+    );
+  }
+
+  // BUSCAR UM POKÉMON
+  static Future<Map<String, dynamic>> buscarDetalhes(int id) async {
+    final resposta = await http.get(
+      Uri.parse(
+        '$url/$id',
+      ),
+    );
+
+    if (resposta.statusCode == 200) {
+      return jsonDecode(
+        resposta.body,
+      );
+    }
+
+    throw Exception(
+      'Erro ao buscar detalhes',
+    );
+  }
+
+  static Future<List<dynamic>> pesquisarPkmn(String busca) async {
+    final resposta = await http.get(
+      Uri.parse(
+        '$url/$busca',
+      ),
+    );
+
+    if (resposta.statusCode == 200) {
+      return jsonDecode(
+        resposta.body,
+      );
+    }
+
+    throw Exception(
+      'Erro ao buscar Pokémon',
+    );
+  }
+}
